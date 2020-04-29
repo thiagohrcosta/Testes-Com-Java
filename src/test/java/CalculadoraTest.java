d7727cd231bfb194520acb530d47ced448bbfc98
Thiago H.R. Costa <thiago_henrique86@hotmail.com>
2020-04-29 16:52:09 -0300

    Realizando testes com Mocks

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,6 +1,8 @@
 import com.test.Calculadora;
 import org.junit.Test;
 
+import static org.mockito.Mockito.mock;
+
 import static org.junit.Assert.*;
 
 public class CalculadoraTest {
@@ -10,5 +12,16 @@ public class CalculadoraTest {
         Calculadora calc = new Calculadora();
         int soma = calc.somar("1+1+3");
         assertEquals(5, soma);
+    }
+
+    @Test
+    public void testeSomarComMock(){
+        Calculadora calculadora = mock(Calculadora.class);
+
+        vhen(calculadora.somar("1+2")).thenReturn(10);
+
+        int resultado = calculadora.somar("1+2");
+
+        assertEquals(10, resultado);
     }
 }
